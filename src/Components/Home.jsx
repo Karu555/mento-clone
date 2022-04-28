1c9bfbea0d7ea4059d8276ba279920f13698b062
Karunesh Ranjan <karunesh029@gmail.com>
2022-04-29 00:33:51 +0530

    Add files via upload

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,7 @@
+import { Footerdown } from "./Footerdown"
+import { FooterTop } from "./Footertop"
 import { Home1 } from "./Home1"
+import { Home2 } from "./Home2"
 import { Navbar } from "./Navbar"
 
 export const Home = () => {
@@ -6,6 +9,9 @@ export const Home = () => {
        <div>
            <Navbar />
            <Home1 />
+           <Home2 />
+           <FooterTop />
+           <Footerdown />
        </div>
    )
 }
